c9c9c5382f094b18dc8c6c3d6e1f37c733491ad1
Brian Kirsten <brian.kirsten.bss@gmail.com>
2014-01-08 14:20:12 -0500

    refactoring some ugly

--- src/fictfact.js
+++ src/fictfact.js
@@ -1,9 +1,11 @@
+var ff_url = 'http://www.fictfact.com';
+
 var fictfact = (function(){
 	return {
 		getBooksReading : function(userid, callback){
 			
 			var request = $.ajax({
-				url: 'http://www.fictfact.com/Ajax/GetBookListByStatus/'+ userid +'/2?callback=' + callback,
+				url: ff_url + '/Ajax/GetBookListByStatus/'+ userid +'/2?callback=' + callback,
 			    type: 'GET',
 			    dataType: 'jsonp',
 				jsonpCallback: callback
